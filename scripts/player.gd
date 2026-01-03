192a84db0d5b6cf0c8782d3e33cf55eb74800626
Rus <SaylerPlay@gmail.com>
2026-01-03 23:57:40 +0500

    initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,75 @@
+extends CharacterBody3D
+
+@onready var camera_mount: Node3D = $camera_mount
+@onready var animation_player: AnimationPlayer = $visuals/mixamo_base/AnimationPlayer
+@onready var visuals: Node3D = $visuals
+
+var SPEED = 3.5
+const JUMP_VELOCITY = 4.5
+
+var walking_speed: float = 3.0
+var running_speed: float = 5.0
+
+var is_running: bool = false
+var is_locked: bool = false
+
+@export var sens_horizontal: float = 0.2
+@export var sens_vertical: float = 0.2
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(event.relative.x * sens_horizontal * -1))
+		visuals.rotate_y(deg_to_rad(event.relative.y * sens_vertical))
+		camera_mount.rotate_x(deg_to_rad(event.relative.y * sens_vertical * -1))
+		
+func _physics_process(delta: float) -> void:
+	if !animation_player.is_playing():
+		is_locked = false
+	
+	if Input.is_action_just_pressed("kick"):
+		if animation_player.current_animation != "kick":
+			animation_player.play("kick")
+			is_locked = true
+	
+	if Input.is_action_pressed("run"):
+		SPEED = running_speed
+		is_running = true
+	else:
+		SPEED = walking_speed
+		is_running = false
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "forward", "backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		if !is_locked:
+			if is_running:
+				if animation_player.current_animation != "running":
+					animation_player.play("running")
+			else:
+				if animation_player.current_animation != "walking":
+					animation_player.play("walking")
+			visuals.look_at(position + direction)
+				
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+	else:
+		if !is_locked:
+			if animation_player.current_animation != "idle":
+				animation_player.play("idle")
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	if !is_locked:
+		move_and_slide()
